181c64365787f3b6754295ba54b67f2eb53d11f1
lpisces <lpisces@ubuntu.office>
2013-03-31 13:15:49 +0800

    app bone scaffold init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bone::Application.config.secret_token = 'ca75f8b8ac92fe42759b6c64dc7921670f94119881f779eb9ca9b6095ffb53c610d5fd7893403d6f46994c38303976618df92231b5394e428017e3860a5c7084'
